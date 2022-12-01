ed19b7551a0b1e164d4c58e323030b6155168af6
Mary Xie <48318559+MARYscu@users.noreply.github.com>
2022-11-30 22:11:00 -0500

    Add files via upload

--- img_detection.py
+++ img_detection.py
@@ -1,6 +1,7 @@
 import cv2
 import pandas as pd
 import numpy as np
+import copy
 
 BERRY_DISTANCE = 10 #unit in pixel
 ZOMBIE_DISTANCE = 30
@@ -52,11 +53,15 @@ def object_info(img, img_width, img_height):
 
 def helper_contour(view, object_data, type):
     add = True
+    if (type == "green" or type == "blue" or type == "aqua" or type == "purple"):
+        limit = 20
+    else:
+        limit = 5
     if (view[type] == []):
         view[type].append(object_data)
     else:
         for i in view[type]:
-            if ((i[0][0] - object_data[0][0]) < 5 and (i[0][1] - object_data[0][1]) < 5):
+            if ((i[0][0] - object_data[0][0]) < limit and (i[0][1] - object_data[0][1]) < limit):
                 i[0][0] = 0.5 * (i[0][0] + object_data[0][0])
                 i[0][1] = 0.5 * (i[0][1] + object_data[0][1])
                 if (i[1] != 0):
@@ -84,6 +89,18 @@ def helper_contour(view, object_data, type):
     return view 
 def helper_contour_add_dir(image, view, img_width, img_height):
     for key, value in view.items():
+        copy_data = copy.deepcopy(value)
+        new_data  = []
+        if (key == "green" or key == "blue" or key == "aqua" or key == "purple"):
+            for object in copy_data:
+                if object[1] > 30:
+                    new_data.append(object)
+            view[key] = new_data
+        if (key == "possible berries"):
+            for object in copy_data:
+                if object[1] > 20:
+                    new_data.append(object)
+            view[key] = new_data
         for object in value:
             if object[0][0] < 0.25 * img_width:
                 object.append("left")
@@ -97,13 +114,14 @@ def helper_contour_add_dir(image, view, img_width, img_height):
 def find_color(given):
     # color group info
     # first value of the each element is the class name
-    color_info = [["red", "rose", "wine"], ["yellow"], ["orange"], ["pink"], ["green"], ["blue"], ["aqua"], ["purple"]]
+    color_info = [["red", "rose", "wine", "dark sienna"], ["yellow"], ["orange"], ["pink"], ["green"], ["blue"], ["aqua"], ["purple"]]
     for color_group in color_info:
             for color in color_group:
                 if (given.find(color) != -1):
                     return color_group[0]
     return " "
 
+# def object_data_fliter(bject_data):
 def zombie_berry_info(object_data, image, img_width, img_height):
     R = 0
     G = 0
@@ -113,12 +131,15 @@ def zombie_berry_info(object_data, image, img_width, img_height):
     view = {"red": [], "yellow": [], "orange": [], "pink": [],
             "green": [], "blue": [], "aqua": [], "purple": [], 
             "possible berries":[], "possible zombies":[],  "wall": []}
+
+    # print(object_data)
+
     for i in range(len(object_data)):
         color_flag = False
         x,y = object_data[i][0]
-        for cx in range(x - 3, x + 3):
+        for cx in range(x - 4, x + 8):
             if color_flag: break
-            for cy in range(y - 3, y + 3):
+            for cy in range(y, y + 1):
                 if (0 < cx < img_width and 0 < cy < img_height):
                     R  += image[cx][cy][0]
                     G  += image[cx][cy][1]
@@ -129,11 +150,14 @@ def zombie_berry_info(object_data, image, img_width, img_height):
                     color = find_color(color)
                     if (color != " "):
                         color_flag = True
-                        print(color)
-                        break      
+                        # print(color)
+                        break     
+        # print(x, y, color_flag) 
         if color_flag == False:
+            if c == 0: c = 1
             color = getColorName(R/c,G/c,B/c)
             color = color.lower()
+            # print(color)
             color = find_color(color)
         
         if (color != " "):
